8631b12e8dcb298a7234ad4df12b3d30b46b9abe
YoloSwag2993 <lijize09@gmail.com>
2025-04-02 08:45:02 +0000

    1

--- app.py
+++ app.py
@@ -27,5 +27,9 @@ def sentiment_result():
     r = textblob.TextBlob(q).sentiment
     return(render_template("sentiment_result.html",r=r))
 
+@app.route("/paynow", methods=["Get","POST"])
+def paynow():
+    return(render_template("paynow.html",r=r))
+
 if __name__ == "__main__":
     app.run()
